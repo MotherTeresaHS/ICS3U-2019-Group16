51be76c5127642e036f6f5e8a7e2386fb11bc295
Ubuntu <trinity.armstrong@mths.ca>
2020-01-17 19:57:03 +0000

    updates jan17

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_lsw5hFEyT', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '6fmpo3bo1gqr831rpg404qpohe' //is this used anywhere?
+    },
+};
