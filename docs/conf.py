4c5e216bf810823eac915661cb56d51a60706d10
Ubuntu <trinity.armstrong@mths.ca>
2020-01-22 19:30:49 +0000

    updates

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = OverAchiever Website'
+project = u'OverAchiever Website'
 copyright = u'2020, Trinity Armstrong'
 
 # The version info for the project you're documenting, acts as replacement for
